f6e59e52eb3e7f8d83549cd5f7e336fa5f47502e
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-10-13 07:48:12 +0330

    Part 2

---
+++ lib/model.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyModel {
+  final int id;
+  final String text1;
+  final String text2;
+  final String image;
+  final Color color;
+
+  MyModel({
+    required this.id,
+    required this.text1,
+    required this.text2,
+    required this.image,
+    required this.color,
+  });
+}
